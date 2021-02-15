6b2216523828622c9e00ad5e11b297245f1e8431
Rekordny <loadingwheel@gmail.com>
2021-02-15 19:08:01 -0400

    Add Fragment files.

--- NetworkAnalysis/app/src/main/java/ca/unb/mobiledev/networkanalysis/MainActivity.java
+++ NetworkAnalysis/app/src/main/java/ca/unb/mobiledev/networkanalysis/MainActivity.java
@@ -1,29 +1,62 @@
 package ca.unb.mobiledev.networkanalysis;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button_wifi_list, button_search, button_speed_test, button_fixer, button_setting;
-
+    private Button button_wifi, button_search, button_test, button_fixer, button_setting;
+    //Scope: Wifi:0, Search:1, Test:2, Fixer:3, Setting:4, Default scope 0 wifi
+    private int Scope = 0;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_wifi_list.findViewById(R.id.button_wifi_list);
-        button_search.findViewById(R.id.button_search);
-        button_speed_test.findViewById(R.id.button_speed_test);
-        button_fixer.findViewById(R.id.button_fixer);
-        button_setting.findViewById(R.id.button_setting);
 
-       // button_wifi_list.setOnClickListener(v -> {
-       //     Intent switch_wifi_list = new Intent(this ,WifiActivity.class);
-        // });
+        //Initialize fragmentManager and add Transaction
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.layout_center, new WifiFragment()).commit();
+
+        //Initialize buttons and assign listeners
+        button_wifi = findViewById(R.id.button_wifi);
+        button_search = findViewById(R.id.button_search);
+        button_test = findViewById(R.id.button_test);
+        button_fixer = findViewById(R.id.button_fixer);
+        button_setting =findViewById(R.id.button_setting);
+        button_wifi.setOnClickListener(v -> {
+            if(Scope != 0){
+                fragmentManager.beginTransaction().replace(R.id.layout_center, new WifiFragment()).commit();
+                Scope = 0;
+            }
+        });
+        button_search.setOnClickListener(v -> {
+            if(Scope != 1){
+                fragmentManager.beginTransaction().replace(R.id.layout_center, new SearchFragment()).commit();
+                Scope = 1;
+            }
+        });
+        button_test.setOnClickListener(v -> {
+            if(Scope != 2){
+                fragmentManager.beginTransaction().replace(R.id.layout_center, new TestFragment()).commit();
+                Scope = 2;
+            }
+        });
+        button_fixer.setOnClickListener(v -> {
+            if(Scope != 3){
+                fragmentManager.beginTransaction().replace(R.id.layout_center, new FixerFragment()).commit();
+                Scope = 3;
+            }
+        });
+        button_setting.setOnClickListener(v -> {
+            if(Scope != 4){
+                fragmentManager.beginTransaction().replace(R.id.layout_center, new SettingFragment()).commit();
+                Scope = 4;
+            }
+        });
     }
 
 }
